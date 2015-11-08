c7be19f3a60cc9ac11c51cbacfe3c9460f15fa30
nimsarr <simon.bass81@gmail.com>
2015-11-07 22:15:37 -0500

    fix?

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -4,42 +4,32 @@ using System.Collections;
 public class PlayerBehavior : MonoBehaviour {
 	public Rigidbody2D rb;
 	public GameObject planet;
-	public GameObject currentRing;
-	public GameObject ring1;
-	public GameObject ring2;
-	
-	
-	private float acceleration = 9.8f;
+	private float acceleration = 100f;
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
-		currentRing = ring1;
+		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKey (KeyCode.Space)) {
-			Debug.Log (5);
-			//Vector3 op = transform.position;
-			//transform.position = new Vector3(op.x, op.y+0.5f, op.z);
-		}
+		//transform.rotation = Quaternion.Euler (0, 0, Mathf.Atan2(transform.position.y, transform.position.x));
 		//transform.Translate (-Vector3.right * 5 * Time.deltaTime);
 		
 		Vector3 posDiff = planet.transform.position - transform.position;
 		Vector3 perpendicular = new Vector3 (-posDiff.y, posDiff.x, posDiff.z);
-		
 		rb.velocity = perpendicular.normalized * 5;
 		
+		
 		rb.AddForce(posDiff.normalized * acceleration);
+		if (Input.GetKey (KeyCode.Space)) {
+			Debug.Log (5);
+			Vector3 n = Quaternion.Euler(0, 0, -30) * -posDiff;
+			rb.velocity = n.normalized*20;
+			//rb.AddForce(n.normalized*20,ForceMode2D.Impulse);
+			//Vector3 op = transform.position;
+			//transform.position = new Vector3(op.x, op.y+0.5f, op.z);
+		}
 		//rb.AddForce (perpendicular.normalized * 5);
 	}
-	
-	void OnCollisionEnter2D(Collision2D obs){
-		if (obs.gameObject.name.Contains("Gap")) {
-			Debug.Log ("gap");
-			currentRing.gameObject.GetComponent<Collider2D>().enabled=false;
-			currentRing=ring2;
-		}
-	}
-	
 }
