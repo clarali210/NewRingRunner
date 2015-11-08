936ca3d71cbd216153598f3f4f3d8cbfe1753507
Clara Li <clarali2000@126.com>
2015-11-07 20:40:12 -0500

    obstacle

---
+++ Assets/Scripts/Obstacles.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacles : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	}
+
+	void OnCollisionEnter2D(Collision2D obs){
+		if (obs.gameObject.name == "Obstacles") {
+			Destroy (this.gameObject);
+		}
+	}
+
+}
+
