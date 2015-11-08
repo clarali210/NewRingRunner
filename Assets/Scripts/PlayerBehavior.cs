05cb90ebba65b00868248e04c73470cfbc7e3361
Sam Tang <17tangs@my.weston.org>
2015-11-07 19:11:06 -0500

    Added player script

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour {
+	public Rigidbody2D rb;
+	public GameObject planet;
+	private float acceleration = -9.8f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce((planet.transform.position - transform.position).normalized * acceleration);
+	}
+}
